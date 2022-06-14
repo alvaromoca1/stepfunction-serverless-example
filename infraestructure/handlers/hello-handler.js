d5e6f3ccb8cc9a965f2ff3dd67d19aa0d82e53d1
alvaromoca1 <alvaromoca1@gmail.com>
2022-06-13 21:32:49 -0500

    add new metho

--- infraestructure/handlers/hello-handler.js
+++ infraestructure/handlers/hello-handler.js
@@ -3,8 +3,41 @@ const responseHttp = require('../helpers/response')
 const { 
   getHelloWord: getHelloController,
 } = require('../../application/controllers/helloController')
+const AWS = require('aws-sdk');
 
-module.exports.getHello = async (event) => {
-  const { success, data, message, statusCode } = await getHelloController();
+module.exports.getHelloStep = async (event) => {
+  const reqBody = event.body || {};
+  const stepfunctions = new AWS.StepFunctions();
+  var params = {
+    stateMachineArn: 'arn:aws:states:us-east-1:776703223828:stateMachine:stepFunctions_test04',
+    input: JSON.stringify(reqBody)
+  }
+  
+
+  const suscribete = await stepfunctions.startExecution(params).promise()
+        .then(async data => {
+            console.debug('==> data: ', data)
+            await new Promise(r => setTimeout(r, 3000));
+            return await stepfunctions.describeExecution({ executionArn: data.executionArn }).promise();
+        })
+        .then(result => {
+          console.debug("reulst debug -> : ",result)
+           return {
+                statusCode: 200,
+                message: JSON.stringify(result)
+            }
+        })
+        .catch(err => {
+            console.error('err: ', err)
+            return {
+                statusCode: 500,
+                message: JSON.stringify({ message: 'facing error' })
+            }
+        })
+console.debug("suscribete -> : ",suscribete);
+
+
+
+  const { success, data, message, statusCode } = await getHelloController(event);
   return responseHttp(success, data, message, statusCode);
 };
